21229296c75f7aa4270ba49313e9c3f315569380
zjakubb <jakub.zuchowicz@student.uj.edu.pl>
2024-01-31 20:27:44 +0100

    initial commit

---
+++ src/test/java/com/zuchowicz/jakub/TicketManager/TicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuchowicz.jakub.TicketManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
